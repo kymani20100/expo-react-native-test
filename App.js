81cf76d49ca834d1d2c66e9c24dc54e3edded4d1
Kymani Emmanuel Glimeti <kymani20100@gmail.com>
2021-05-21 15:02:50 +0000

    Second mobile App

--- App.js
+++ App.js
@@ -1,25 +1,21 @@
-import React,{useState} from 'react';
-import { Text, View, StyleSheet,Button } from 'react-native';
+import React from 'react';
+import { Text, View, StyleSheet, Button, TextInput } from 'react-native';
 import Constants from 'expo-constants';
 
-// You can import from local files
-import AssetExample from './components/AssetExample';
+// // You can import from local files
+// import AssetExample from './components/AssetExample';
 
-// or any pure javascript modules available in npm
-import { Card } from 'react-native-paper';
+// // or any pure javascript modules available in npm
+// import { Card } from 'react-native-paper';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('Change code in the editor and watch it change on your phone! Save to get a shareable url');
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.paragraph}>
-        {outputText}
-      </Text>
-      <Card>
-        <AssetExample />
-      </Card>
-      <Button title="Change" onPress={() => setOutputText('I was born by the river. In a little tent.')} />
+    <View>
+      <View>
+        <TextInput />
+        <Button title="ADD " />
+      </View>
     </View>
   );
 }
