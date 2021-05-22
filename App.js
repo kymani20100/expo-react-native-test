213d454adb4b55314be924fda9e50d2d1a9b6092
Kymani Emmanuel Glimeti <kymani20100@gmail.com>
2021-05-22 16:11:53 +0000

    Changes to the M App

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
-// import Constants from 'expo-constants';
+import Constants from 'expo-constants';
 
 import Header from './components/Header';
 export default function App() {
