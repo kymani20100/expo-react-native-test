798abf9fe75a4e67a1fc005e55c1c112039e7f2f
Kymani Emmanuel Glimeti <kymani20100@gmail.com>
2021-05-22 16:07:02 +0000

    Changes to the M App

--- App.js
+++ App.js
@@ -1,38 +1,18 @@
 import React from 'react';
-import { Text, View, StyleSheet, Button, TextInput } from 'react-native';
-import Constants from 'expo-constants';
+import {View, StyleSheet} from 'react-native';
+// import Constants from 'expo-constants';
 
-// // You can import from local files
-// import AssetExample from './components/AssetExample';
-
-// // or any pure javascript modules available in npm
-// import { Card } from 'react-native-paper';
-
+import Header from './components/Header';
 export default function App() {
-
   return (
-    <View>
-      <View style={{padding: 40}}>
-        <TextInput placeholder="XoXo" 
-        style={{borderBottomColor: 'black', borderBottomWidth: 1}} />
-        <Button title="ADD " />
-      </View>
+    <View style={styles.screen}>
+      <Header title="Braniac" />
     </View>
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     paddingTop: Constants.statusBarHeight,
-//     backgroundColor: '#ecf0f1',
-//     padding: 8,
-//   },
-//   paragraph: {
-//     margin: 24,
-//     fontSize: 18,
-//     fontWeight: 'bold',
-//     textAlign: 'center',
-//   },
-// });
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  }
+});
